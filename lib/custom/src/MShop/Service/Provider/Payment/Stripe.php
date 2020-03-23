047275a497700c74b4d8ae8a96b80f02669ba1f6
Aimeos <aimeos@aimeos.org>
2020-03-23 13:01:31 +0100

    Adds Stripe token only if available

--- lib/custom/src/MShop/Service/Provider/Payment/Stripe.php
+++ lib/custom/src/MShop/Service/Provider/Payment/Stripe.php
@@ -157,13 +157,16 @@ class Stripe
 	 */
 	protected function getData( \Aimeos\MShop\Order\Item\Base\Iface $base, $orderid, array $params )
 	{
+		$session = $this->getContext()->getSession();
 		$data = parent::getData( $base, $orderid, $params );
 
 		if( isset( $params['paymenttoken'] ) ) {
-			$this->getContext()->getSession()->set( 'stripe_token', $params['paymenttoken'] );
+			$session->set( 'aimeos/stripe_token', $params['paymenttoken'] );
 		}
 
-		$data['token'] = $this->getContext()->getSession()->get( 'stripe_token' );
+		if( ( $token = $session->get( 'aimeos/stripe_token' ) ) !== null ) {
+			$data['token'] = $token;
+		}
 
 		return $data;
 	}
